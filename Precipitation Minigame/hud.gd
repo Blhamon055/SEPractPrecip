8e77d05b59bd33c62ca40899585968c2f58e75ef
lukevaughn20 <78869404+lukevaughn20@users.noreply.github.com>
2024-02-19 15:10:11 -0600

    Added basic clickmap

---
+++ Precipitation Minigame/hud.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process_input(true)
+
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if(event is InputEventMouseButton):
+		print("clicked")
