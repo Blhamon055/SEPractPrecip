2e3bfe1d5f774fef3083fad4353a1828106c24ea
Braeden Lhamon <S543387@nwmissouri.edu>
2024-02-19 14:09:27 -0600

    Working on Jump functionality

--- Precipitation Minigame/CharacterBody2D.gd
+++ Precipitation Minigame/CharacterBody2D.gd
@@ -7,8 +7,6 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
-
 """
 func _physics_process(delta):
 	velocity.y += gravity * delta
@@ -37,7 +35,7 @@ func _physics_process(delta):
 	velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up"):
 		velocity.y = SPEED
 
 	# Get the input direction.
