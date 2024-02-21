c2b3d7987a79c6564a9c0099c2fa6d68c1341ecb
lukevaughn20 <78869404+lukevaughn20@users.noreply.github.com>
2024-02-21 15:00:05 -0600

    updated click map

--- Precipitation Minigame/hud.gd
+++ Precipitation Minigame/hud.gd
@@ -1,6 +1,5 @@
 extends CanvasLayer
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process_input(true)
@@ -9,4 +8,5 @@ func _ready():
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if(event is InputEventMouseButton):
-		print("clicked")
+		if(event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
+			print("clicked")
