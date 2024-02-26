758ac692b209f3e7d633a2f9f2b21879dfcf138a
lukevaughn20 <78869404+lukevaughn20@users.noreply.github.com>
2024-02-26 14:53:25 -0600

    added function to check for correct answer
    Added a basic function to generate a random number and have it assign which box is  the correct answer to hit

--- Precipitation Minigame/hud.gd
+++ Precipitation Minigame/hud.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
+var rng = RandomNumberGenerator.new()
+var my_rnd_num = rng.randi_range(1,2)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process_input(true)
@@ -9,4 +12,15 @@ func _ready():
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if(event is InputEventMouseButton):
 		if(event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
-			print("clicked", shape_idx)
+			if(_rng_selector(shape_idx) == true):
+				print("clicked the right box")
+			else:
+				print("clicked wrong box")
+
+
+func _rng_selector(x):
+	if(x == my_rnd_num):
+		return true
+	else:
+		return false
+	
