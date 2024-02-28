b54b40a334fb61fe643dbb0a4a5e4491ba81d161
lukevaughn20 <78869404+lukevaughn20@users.noreply.github.com>
2024-02-28 15:00:52 -0600

    started science

---
+++ Precipitation Minigame/Main.gd
@@ -0,0 +1,12 @@
+extends Node2D
+var chemEq = {"1": ["Cl- K+","AgBr,MgCO3,Hg2(NO3)2"]}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
