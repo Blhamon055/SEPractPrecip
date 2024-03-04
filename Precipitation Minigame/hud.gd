2e474178ab55acd44f67f620bc4213288247b059
danielgatschet <danielgatschet@gmail.com>
2024-03-04 15:20:13 -0600

    Added where the bridge can disapear but cant get collision to go away

--- Precipitation Minigame/hud.gd
+++ Precipitation Minigame/hud.gd
@@ -14,6 +14,7 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 	if(event is InputEventMouseButton):
 		if(event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
 			if(_rng_selector(shape_idx) == true):
+				get_node("/root/Main/Bridge").hide()
 				print("clicked the right box")
 			else:
 				print("clicked wrong box")
