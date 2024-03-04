ba0387e7b10ccff760eae5327f025b978fae1c5c
lukevaughn20 <78869404+lukevaughn20@users.noreply.github.com>
2024-03-04 14:41:26 -0600

    added more compounds

--- Precipitation Minigame/Main.gd
+++ Precipitation Minigame/Main.gd
@@ -1,7 +1,7 @@
 extends Node2D
-var chemSo = ["Cl-,K+", "C2042-,Na+,Na+"]
-var chemCo = ["AgBr,MgCO3,Hg2(NO3)2", "CaSO4,CaCL2,KCl"]
-var chemAws = ["Hg2(NO3)2", "CaCl2"]
+var chemSo = ["Cl-,K+", "C2042-,Na+,Na+", "K+,F-", "Al3+,Cl-,Cl-,Cl-", "CO2-3,Na+,Na+", "Pb2+,NO-2, NO-2", "Ca2+,Br-,Br-", "K+,NO3-", "Fe3+,Cl-,Cl-,Cl-","Na+,Na+,Na+,PO3-4"]
+var chemCo = ["AgBr,MgCO3,Hg2(NO3)2", "CaSO4,CaCL2,KCl", "MgS04,CuS04,Ca2(PO4)2", "NaOH,Ba3(PO4)2,KClO3", "NH4I,Fe(OH)3,CaO","ZnClO4,Na3PO4,NiS", "K2CO3,MgCl2,NH4C2H3O2", "FeCl3,Al2(SO4)3,Ag2CO3", "KSCN,NaOH,LiC2h3O2", "NH4F,KI,CaCl2"]
+var chemAws = ["Hg2(NO3)2", "CaCl2","MgS04","NaOH","CaO", "Na3PO4", "K2CO3","none", "NaOH", "CaCl2"]
 
 
 
