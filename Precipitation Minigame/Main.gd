313308814f74d36eccfe61bab4338312b6951d59
Nic Black <78877662+NicBB5@users.noreply.github.com>
2024-03-18 15:06:46 -0500

    Finished formulas
    finished added formulas in to arrays

--- Precipitation Minigame/Main.gd
+++ Precipitation Minigame/Main.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-var chemSo = ["Cl-,K+", "C2042-,Na+,Na+", "K+,F-", "Al3+,Cl-,Cl-,Cl-", "CO2-3,Na+,Na+", "Pb2+,NO-2, NO-2", "Ca2+,Br-,Br-", "K+,NO3-", "Fe3+,Cl-,Cl-,Cl-","Na+,Na+,Na+,PO3-4"]
-var chemCo = ["AgBr,MgCO3,Hg2(NO3)2", "CaSO4,CaCL2,KCl", "MgS04,CuS04,Ca2(PO4)2", "NaOH,Ba3(PO4)2,KClO3", "NH4I,Fe(OH)3,CaO","ZnClO4,Na3PO4,NiS", "K2CO3,MgCl2,NH4C2H3O2", "FeCl3,Al2(SO4)3,Ag2CO3", "KSCN,NaOH,LiC2h3O2", "NH4F,KI,CaCl2"]
-var chemAws = ["Hg2(NO3)2", "CaCl2","MgS04","NaOH","CaO", "Na3PO4", "K2CO3","none", "NaOH", "CaCl2"]
+var chemSo = ["Cl-,K+", "C2042-,Na+,Na+", "K+,F-", "Al3+,Cl-,Cl-,Cl-", "CO2-3,Na+,Na+", "Pb2+,NO-2, NO-2", "Ca2+,Br-,Br-", "K+,NO3-", "Fe3+,Cl-,Cl-,Cl-","Na+,Na+,Na+,PO3-4", "K+, K+, K+, AsO3-4", "Cd2+, SO2-4", "Cr3+, SCN-, SCN-, SCN-", "Li+, ClO-4", "Ag+, NO-3", "K+, K+, CrO2-4", "OH-, PO3-4, Na+, Na+, Na+, Na+", "AsO3-4, OH-, Na+, Na+, Na+, Na+", "Hg2+, ClO-4, ClO-4", "Hg2+2, NO-3, NO-3"]
+var chemCo = ["AgBr,MgCO3,Hg2(NO3)2", "CaSO4,CaCL2,KCl", "MgS04,CuS04,Ca2(PO4)2", "NaOH,Ba3(PO4)2,KClO3", "NH4I,Fe(OH)3,CaO","ZnClO4,Na3PO4,NiS", "K2CO3,MgCl2,NH4C2H3O2", "FeCl3,Al2(SO4)3,Ag2CO3", "KSCN,NaOH,LiC2h3O2", "NH4F,KI,CaCl2", "FeCl3, NaBr, KF", "NaC2H3O2, K2SO4, Na2S", "NaOH, KClO4, Na2SO4", "CuO, AlBr3, Ca(OH)2", "NaCl, KF, Ca(ClO4)2", "KCl, LiBr, BaCl2", "NaOH, Ca(NO3)2, KBr", "CuCl2, KC3H3O2, NaI", "K2S, KBr, NaCl", "Na2CO3, KClO4, LiNO3"]
+var chemAws = ["Hg2(NO3)2", "CaCl2","MgSO4","NaOH","CaO", "Na3PO4", "K2CO3","none", "NaOH", "CaCl2", "NaCl", "BaCl2", "Ca(NO3)2", "CuCl2", "K2S", "Na2CO3"]
 
 var rng = RandomNumberGenerator.new()
 var numbers = [0,1,2,3,4,5,6,7,8,9]
